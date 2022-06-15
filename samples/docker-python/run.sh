e8ff45d5e8fcb633e4b0b92f5521aa2ce81338b8
Apollo3zehn <git@m1.apollo3zehn.net>
2022-06-15 16:38:31 +0200

    Fix sample.

--- samples/docker-python/run.sh
+++ samples/docker-python/run.sh
@@ -1,5 +1,12 @@
-echo "Hallo"
+green=$'\e[0;32m'
+white=$'\e[0m'
+echo "${green}Welcome to the nexus-python setup run script"'!'"${white}"
+echo "${green}Setting up SSH ..."${white}"
 
+apt update
+apt install openssh-server -y
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+service ssh start
 
 trap : TERM INT
 sleep infinity & wait
