4a476e4b9869a11b52f1b2a4acdc1201751e95ad
Apollo3zehn <git@m1.apollo3zehn.net>
2022-06-15 17:03:32 +0200

    Fix sample.

--- samples/docker-python/setup-python.sh
+++ samples/docker-python/setup-python.sh
@@ -1,14 +1,14 @@
 green=$'\e[0;32m'
 white=$'\e[0m'
-echo "${green}Welcome to the nexus-python run script"'!'"${white}"
-echo "${green}Setting up SSH ...${white}"
+echo "${green}Welcome to the setup-python script"'!'"${white}"
+echo "${green}Setting up SSH server ...${white}"
 
 apt update
 apt install openssh-server -y
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 service ssh start
 
-echo "${green}Setting up SSH ... Done.${white}"
+echo "${green}Setting up SSH server ... Done.${white}"
 
 trap : TERM INT
 sleep infinity & wait
---
+++ samples/docker-python/setup-test.sh
@@ -0,0 +1,12 @@
+green=$'\e[0;32m'
+white=$'\e[0m'
+echo "${green}Welcome to the setup-test script"'!'"${white}"
+echo "${green}Setting up SSH client ...${white}"
+
+apt update
+apt install openssh-client -y
+
+echo "${green}Setting up SSH client ... Done.${white}"
+
+trap : TERM INT
+sleep infinity & wait
