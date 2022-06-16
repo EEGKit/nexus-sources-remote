daa829c98f66d18a464a4700cc8e956b3cc3881f
Apollo3zehn <git@m1.apollo3zehn.net>
2022-06-16 17:37:18 +0200

    Fix sample.

--- setup/docker/python/setup-main.sh
+++ setup/docker/python/setup-main.sh
@@ -1,10 +1,14 @@
 green=$'\e[0;32m'
 white=$'\e[0m'
 echo "${green}Welcome to the setup-test script"'!'"${white}"
-echo "${green}Set up SSH client${white}"
 
+echo "${green}Set up SSH client${white}"
 apt update
 apt install openssh-client -y
+
+echo "Clone repository ${green}https://github.com/Nexusforge/nexus-sources-remote${white}"
+git clone https://github.com/Nexusforge/nexus-sources-remote
+cd nexus-sources-remote
 
 trap : TERM INT
 sleep infinity & wait
---
+++ tests/Nexus.Sources.Remote.Tests/SetupDockerPythonTests.cs
@@ -0,0 +1,80 @@
+using Microsoft.Extensions.Logging.Abstractions;
+using Nexus.DataModel;
+using Nexus.Extensibility;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Xunit;
+
+namespace Nexus.Sources.Tests
+{
+    public class SetupDockerPythonTests
+    {
+#if LINUX
+        [Fact]
+#endif
+        public async Task CanReadFullDay()
+        {
+            var dataSource = new Remote() as IDataSource;
+            var command = "python main.py nexus-main {remote-port}";
+            var context = CreateContext(command);
+
+            await dataSource.SetContextAsync(context, NullLogger.Instance, CancellationToken.None);
+
+            var catalog = await dataSource.GetCatalogAsync("/A/B/C", CancellationToken.None);
+            var resource = catalog.Resources!.First();
+            var representation = resource.Representations!.First();
+
+            var catalogItem = new CatalogItem(
+                catalog with { Resources = default! }, 
+                resource with { Representations = default! }, 
+                representation);
+
+            var begin = new DateTime(2020, 01, 01, 0, 0, 0, DateTimeKind.Utc);
+            var end = new DateTime(2020, 01, 01, 0, 0, 10, DateTimeKind.Utc);
+            var (data, status) = ExtensibilityUtilities.CreateBuffers(representation, begin, end);
+
+            var length = 10;
+            var expectedData = new double[length];
+            var expectedStatus = new byte[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                expectedData[i] = i;
+            }
+
+            expectedStatus.AsSpan().Fill(1);
+
+            var request = new ReadRequest(catalogItem, data, status);
+            await dataSource.ReadAsync(begin, end, new ReadRequest[] { request }, default!, new Progress<double>(), CancellationToken.None);
+            var doubleData = new CastMemoryManager<byte, double>(data).Memory;
+
+            Assert.True(expectedData.SequenceEqual(doubleData.ToArray()));
+            Assert.True(expectedStatus.SequenceEqual(status.ToArray()));
+        }
+
+        private DataSourceContext CreateContext(string command)
+        {
+            return new DataSourceContext(
+                ResourceLocator: new Uri("file:///" + Path.Combine(Directory.GetCurrentDirectory(), "TESTDATA")),
+                SystemConfiguration: new JsonObject()
+                {
+                    ["remote-templates"] = new JsonObject()
+                    {
+                        ["local"] = "ssh root@nexus-python run-python-root.sh {git-url} {command}",
+                    }
+                }.Deserialize<JsonElement>(),
+                SourceConfiguration: new JsonObject()
+                {
+                    ["listen-address"] = "0.0.0.0",
+                    ["template"] = "local",
+                    ["command"] = command,
+                    ["environment-variables"] = new JsonObject()
+                    {
+                        ["PYTHONPATH"] = $"{Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "..", "src", "remoting", "python-remoting")}"
+                    }
+                }.Deserialize<JsonElement>(),
+                RequestConfiguration: default
+            );
+        }
+    }
+}
