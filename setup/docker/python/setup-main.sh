abef2a2bb87bd2c445a06b40835ffff0a2132cc4
Apollo3zehn <git@m1.apollo3zehn.net>
2022-06-16 17:43:56 +0200

    Fix setup.

--- setup/docker/python/setup-main.sh
+++ setup/docker/python/setup-main.sh
@@ -1,6 +1,6 @@
 green=$'\e[0;32m'
 white=$'\e[0m'
-echo "${green}Welcome to the setup-test script"'!'"${white}"
+echo "${green}Welcome to the setup-main script"'!'"${white}"
 
 echo "${green}Set up SSH client${white}"
 apt update
