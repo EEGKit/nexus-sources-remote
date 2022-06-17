6272859dc56273d303d82952b965a3dbe5976fa1
Apollo3zehn <git@m1.apollo3zehn.net>
2022-06-17 09:51:50 +0000

    Improvements.

---
+++ tests/Nexus.Sources.Remote.Tests/SetupDockerPythonTests.sh
@@ -0,0 +1,16 @@
+setup_folder="setup/docker"
+satellite_id="python"
+
+bash "${setup_folder}/setup-host.sh" $satellite_id
+docker-compose --file ${setup_folder} --env-file "${setup_folder}/${satellite_id}/.env" up -d
+
+while true; do
+   docker exec "nexus-main" test -f "/var/lib/nexus/ready" && \
+   docker exec "nexus-${satellite_id}" test -f "/var/lib/nexus/ready" && \
+   break
+
+   echo "Waiting for Docker containers to become ready ..."
+   sleep 1;
+done
+
+docker exec nexus-main bash -c "cd /root/nexus-sources-remote; dotnet test --filter Nexus.Sources.Tests.SetupDockerPythonTests"
