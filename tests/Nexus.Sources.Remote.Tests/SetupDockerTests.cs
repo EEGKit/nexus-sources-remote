151aae79582dfbdff4d28128aa98bac2d24f7a09
Apollo3zehn <git@m1.apollo3zehn.net>
2022-07-15 17:27:21 +0200

    Fix test.

--- tests/Nexus.Sources.Remote.Tests/SetupDockerTests.cs
+++ tests/Nexus.Sources.Remote.Tests/SetupDockerTests.cs
@@ -57,7 +57,7 @@ namespace Nexus.Sources.Tests
         private DataSourceContext CreateContext(string satelliteId, string command)
         {
             return new DataSourceContext(
-                ResourceLocator: new Uri("https://example.com")),
+                ResourceLocator: new Uri("https://example.com"),
                 SystemConfiguration: new JsonObject()
                 {
                     ["remote-templates"] = new JsonObject()
