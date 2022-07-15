6d934998c036b45c4ea193d7a96ef1795444851d
Apollo3zehn <20972129+Apollo3zehn@users.noreply.github.com>
2022-07-15 14:37:38 +0200

    Update satellite.sh

--- setup/docker/dotnet/satellite.sh
+++ setup/docker/dotnet/satellite.sh
@@ -1,3 +1,4 @@
+red=$'\e[0;31m'
 green=$'\e[0;32m'
 orange=$'\e[0;33m'
 white=$'\e[0m'
@@ -5,9 +6,17 @@ white=$'\e[0m'
 echo "${green}Welcome to the satellite.sh script!${white}"
 project=$2
 
-if [ -f "../commit_changed" ]; then
+if [ -f "../commit_changed" || ! -f "../build_successful" ]; then
+
     echo "Build project ${green}${project}${white}"
     dotnet build -c Release ${project}
+
+    if [[ $? == 0 ]]; then
+        touch "../build_successful"
+    else
+        echo "${red}Build failed${white}"
+        rm --force "../build_successful"
+    fi
 fi
 
 # run user code
